59b767bd770a6b1e4a69a76d7cf2631c5da41249
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-10-10 06:33:16 -0300

    FEAT: add function to delete a task

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,8 +1,22 @@
+import { AiFillDelete } from "react-icons/ai";
+import axios from "axios";
+import { useAlert } from "react-alert";
+
 import "./TaskItem.scss";
 
-import { AiFillDelete } from "react-icons/ai";
-
 const TaskItem = ({ task }) => {
+    const alert = useAlert();
+
+    const handleTaskDeletion = async () => {
+        try {
+            await axios.delete(
+                `https://fsc-task-manager-backend.herokuapp.com/tasks/${task._id}`
+            );
+        } catch (error) {
+            alert.error("errr");
+        }
+    };
+
     return (
         <div className="task-item-container">
             <div className="task-description">
@@ -26,7 +40,11 @@ const TaskItem = ({ task }) => {
             </div>
 
             <div className="delete">
-                <AiFillDelete size={18} color="#F97474" />
+                <AiFillDelete
+                    size={18}
+                    color="#F97474"
+                    onClick={handleTaskDeletion}
+                />
             </div>
         </div>
     );
